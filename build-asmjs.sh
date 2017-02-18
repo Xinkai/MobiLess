252d7d4527d686cfa0799d74412ff7d344984ef0
Xinkai Chen <xinkai.chen@qq.com>
2017-02-19 03:02:30 +0800

    Fix: Some flags break the build

--- build-asmjs.sh
+++ build-asmjs.sh
@@ -3,4 +3,4 @@
 set -e
 
 cargo build --target asmjs-unknown-emscripten --verbose --release --lib
-emcc -O3 -g0 --closure 1 --memory-init-file 0 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS="['_process_mobi_file']" target/asmjs-unknown-emscripten/release/libmobiless.a -o mobiless.js
+emcc -O3 -g0 --memory-init-file 0 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS="['_process_mobi_file']" target/asmjs-unknown-emscripten/release/libmobiless.a -o mobiless.js
